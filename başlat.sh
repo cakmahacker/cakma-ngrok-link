688d3d9f0a452d77c6b19d13e79959131bea9a4d
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2022-01-28 15:50:19 +0300

    Update başlat.sh

--- başlat.sh
+++ başlat.sh
@@ -2,13 +2,13 @@ pkg install python3
 pkg install figlet
 clear
 figlet 3
-sleep(1)
+sleep 1
 clear
 figlet 2
-sleep(1)
+sleep 1
 clear
 figlet 1
-sleep(1)
+sleep 1
 clear
 figlet Yönlendirilir...
 python3 cakma-ngrok-link.py
